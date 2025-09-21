838aa0e6a0d01622860c1b3b063a55a01c7ec490
Salman Zulfiqar Shaikh <salmanzulfiqar04@gmail.com>
2025-09-22 04:38:12 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-amber-950'>Your A-I Powered Resume Builder</h1>
+    </>
+  )
+}
+
+export default App
